4668c9424a24ef72419e746fce8bb0de58bdce4b
Dipti Shevalekar <shevalekar.d@northeastern.edu>
2024-04-03 16:58:49 -0400

    Updating cloud function

--- index.js
+++ index.js
@@ -1,110 +1,60 @@
-const functions = require('@google-cloud/functions-framework');
-
 const formData = require('form-data');
 const Mailgun = require('mailgun.js');
 const mailgun = new Mailgun(formData);
-const mysql = require("mysql");
-
-const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY});
-
-
-const pool = mysql.createConnection({
-  HOST: process.env.HOST,
-  DB_USERNAME: process.env.DB_USERNAME,
-  PASSWORD: process.env.PASSWORD,
-  DATABASE: process.env.DATABASE,
+const functions = require('@google-cloud/functions-framework');
+const mg = mailgun.client({ username: 'api', key: "process.env.MAILGUN_API_KEY" });
+ 
+const mysql = require('mysql');
+ 
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
 });
-
-pool.connect(function (err) {
+ 
+connection.connect(function (err) {
   if (err) {
-    console.error('Error connecting to MySQL: ' + err.stack);
+    console.error('Error connecting to MySQL ---------- ' + err.stack);
     return;
   }
-  console.log('Connected to MySQL as id ' + pool.threadId);
+  console.log('Connected to MySQL ----------- ' + connection.threadId);
 });
-
+ 
 functions.cloudEvent('emailVerificationSubscriber', async (cloudEvent) => {
-  try{
-console.log("Pool Created Successfully")
-
-  
-  const base64name = JSON.parse(Buffer.from(cloudEvent.data.message.data, 'base64').toString())
-   console.log(`${base64name}`)
-   //const {id, UserName, timestamp} = base64name;
-
-console.log("Reached after base64name")
-  //  const UserName = base64name["UserName"];
-  //  const id = base64name["id"];
-  //  const timestamp = base64name["timestamp"];
-
-  const { id, UserName, timestamp } = base64name;
-
-  console.log(base64name)
-  const verificationLink = `http://diptishevalekar.online:3000/v1/user/verifyaccount?token=${id}&timestamp=${timestamp}`;
-
-//    const updateusers = {
-//     id: base64name["id"],
-//     timestamp: new Date().toISOString().slice(0,19).replace('T',' '),
-//    }
-// console.log("I am before try")
-
-//    try {
-//     const queryPromise = new Promise((resolve, reject) => {
-//       pool.query(
-//        ` UPDATE users SET EmailSentTime= ${updateusers.timestamp} WHERE id= ${updateusers.id}` );
-//         (error, results, fields) => {
-//           if (error) {
-//             console.error(error);
-//             reject(error);
-//           }
-//           console.log("Inserted " + results.affectedRows + " row(s).");
-//           resolve();
-//         }
-//        } );
+  try {
+    const base64name = cloudEvent.data.message.data;
+ 
+    const name = base64name
+      ? Buffer.from(base64name, 'base64').toString()
+      : 'World';
+ 
+    console.log(name);
+ 
+    const { id, userName } = JSON.parse(name);
+ 
+    const activationLink = `https://diptishevalekar.online/v1/user/verifyaccount?token=${id}`;
+ 
+    const EmailSentTime = new Date();
     
-//     await queryPromise;
-
-
-//    console.log(`${id} this is the timestamp${timestamp}`);
-  // const verificationLink = `http://localhost:3002/v1/user/verifyaccount?token=${id}&timestamp=${timestamp}`;
-
-
-    const msg = await mg.messages.create('diptishevalekar.online', {
+    connection.query('UPDATE users SET EmailSentTime=? WHERE UserName=?',
+      [EmailSentTime, userName],
+      function (error, results, fields) {
+        if (error) throw error;
+        console.log('Verification details updated for email --------------', userName);
+      });
+ 
+ 
+    const message = await mg.messages.create('diptishevalekar.online', {
       from: "<mailgun@diptishevalekar.online>",
-      to: ["shevalekar.d@northeastern.edu"],
-      subject: "Please verify you email id",
-      text: `Hello ${UserName},
-
-      Welcome to the website!
-
-      Thank you for your interest. Please click on the following link to verify your email address: ${verificationLink}
-      Best Regards`,
-      html: `<p>Hello ${UserName},</p>
-
-      <p>Welcome to the website!</p>
-
-      <p>Thank you for your interest. Please click on the following link to verify your email address: <a href="${verificationLink}">Verify Email</a> </p>
-      
-      <p>Best Regards</p>`
-    });
-
-    console.log(`${id}`);
-
-    const queryString = 'UPDATE users SET EmailSentTime = NOW() WHERE id = ?';
-  await new Promise((resolve, reject) => {
-    pool.query(queryString, [id], (err, results) => {
-      if (err) {
-        console.error('Error updating verificationSentAt:', err);
-        reject(err);
-      } else {
-        console.log('verificationSentAt updated successfully:', results);
-        resolve();
-      }
-    });
-  });
-
+      to: [userName],
+      subject: "Verify your email address",
+      text: `Hi ${userName}, Thank you for your interest. Please click on the following link to verify your email address: ${activationLink}`,
+      html: `<p>Hello ${userName},Thank you for your interest. Please click on the following link to verify your email address: <a href="${activationLink}">Verify Email</a></p>`
+    })
+      .then(msg => console.log(msg)) 
+      .catch(err => console.log(err));
   } catch (error) {
-    console.error(error); 
+    console.error(error);
   }
-
 });
